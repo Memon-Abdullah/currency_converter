e455fe3b8a6c8970052a5c162b0affd081ef5244
Abdullah Memon <abdullahmemon.bscsf21@iba-suk.edu.pk>
2024-02-05 15:24:21 +0500

    add style to all elements

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("script is working");
+
+const amount = document.getElementById("amount");
+const from_select = document.getElementById("from_currency");
+const to_select = documnet.getElementById("to_currency");
+const reaultParagraph = document.getElementById("result");
+const button = document.getElementById("btn");
